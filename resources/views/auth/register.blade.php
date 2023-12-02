1546a8f3881502d1cdc4673c9f7a99273c43102b
Michael-Njoroge <mikethecoder12@gmail.com>
2023-12-02 17:30:29 +0300

    restructure registration form and perform validation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,123 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SignUp</title>
-    <link rel="stylesheet" href="{{asset('../../../assets/style.css')}}">
 
-</head>
- 
 
-<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
-<!-- Styling the login page -->
-
 <style>
-@import url('https://fonts.googleapis.com/css?family=Montserrat:400,500&display=swap');
-body {
-	font-family: Montserrat,Arial, Helvetica, sans-serif;
-	background-color:#f7f7f7;
-}
-* {box-sizing: border-box}
-
-/* Add padding to container elements */
-.container {
-    padding: 20px;
-      width:500px;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-      border:1px solid #ccc;
-      border-radius:10px;
-      background:white;
-  -webkit-box-shadow: 2px 1px 21px -9px rgba(0,0,0,0.38);
-  -moz-box-shadow: 2px 1px 21px -9px rgba(0,0,0,0.38);
-  box-shadow: 2px 1px 21px -9px rgba(0,0,0,0.38);
+  span{
+    color: red;
   }
-
-/* Full-width input fields */
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 15px;
-  margin: 5px 0 12px 0;
-  display: inline-block;
-  border: none;
-  background: #f7f7f7;
-	font-family: Montserrat,Arial, Helvetica, sans-serif;
-}
- 
-input[type=text]:focus, input[type=password]:focus, input[type=phone]:focus, select:focus {
-  background-color: #ddd;
-  outline: none;
-}
- 
-
-/* Set a style for all buttons */
-button {
-  background-color: #0eb7f4;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-  opacity: 0.9;
-	font-size:16px;
-	font-family: Montserrat,Arial, Helvetica, sans-serif;
-	border-radius:10px;
-}
-
-button:hover {
-  opacity:1;
-}
-
-/* Change styles for signup button on extra small screens */
-@media screen and (max-width: 300px) {
-  .signupbtn {
-     width: 100%;
-  }
-}
 </style>
 
-<!-- End of styling the login page -->
-
-
-<body>
-    <form id="register_form" >
+     <form id="register_form" >
         @csrf
-        <div class="container">
+        <div style="text-align: center;">
           <h1 style="text-align: center;">Sign Up</h1>
           <!-- <p>Please fill in this form to create an account.</p> -->
-         <div class="form-controller"> 
-          <label for="email"><b>Name</b></label>
+           <label for="email"><b>Name</b></label>
           <input type="text" name="name" id="name" placeholder="Enter name" >
-          </div>
+          <br>
+          <span class="error name_err"></span>
+          <br><br> 
 
-          <div class="form-controller "> 
-          <label for="email"><b>Email</b></label>
-          <input type="text" placeholder="Enter Email" name="email" id="email" >
-          </div>
-
-          <div class="form-controller">
+           <label for="email"><b>Email</b></label>
+          <input type="email" placeholder="Enter Email" name="email" id="email" >
+          <br>
+          <span class="error email_err"></span>
+          <br><br> 
+         
           <label for="psw"><b>Password</b></label>
           <input type="password" placeholder="Enter Password" name="password" id="password" >
-          </div>
+          <br>
+          <span class="error password_err"></span>
+          <br><br> 
 
           <label for="psw"><b>Confirm Password</b></label>
-          <input type="password" placeholder="Confirm Password" name="password_confirmation"  >
-           
-          <div class="clearfix">
-       
-            <button type="submit" class="btn">Sign Up</button>
-          </div>
-        </div>
+          <input type="password" placeholder="Confirm Password" name="password_confirmation">
+          <br>
+          <span class="error password_confirmation_err"></span>
+          <br><br> 
+
+          <button type="submit" class="btn">Sign Up</button>
+         </div>
       </form>
-      
-      <script src="{{asset('../../../assets/validate.min.js')}}"></script>
+      <p class="result" style="text-align: center;"></p>
 
       <script type="text/javascript">
         $(document).ready(function(){
@@ -133,57 +56,37 @@ button:hover {
                 success:function(data){
                     console.log(data);
                     if(data.message){
-
-                    }                
+                      $("#register_form")[0].reset();
+                      $(".error").text("");
+                      $(".result").text(data.message)
+                    } else{
+                      errorMessage(data);
+                    } 
                 }
             });
         });
+        function errorMessage(message){
+          $(".error").text("");
+          $.each(message, function (key,value){
+            if(key == 'password'){
+              if(value.length > 1){
+                $(".password_err").text(value[0]);
+                $(".password_confirmation_err").text(value[1]);
+              }else{
+                if(value[0].includes('password confirmation')){
+                  $(".password_confirmation_err").text(value);
+                }else{
+                  $(".password_err").text(value);
+                }
+              }
+            }else{
+              $("."+key+"_err").text(value);
+
+            }
+          })
+        }
     });
   
     </script>
 
-<script type="text/javascript">
-    $(document).ready(function (){
-        $('#register_form').validate({
-            rules: {
-                name: {
-                    required : true,
-                }, 
-                email: {
-                    required : true,
-                }, 
-                password: {
-                    required : true,
-                }, 
-                
-            },
-            messages :{
-               name: {
-                    required : 'Name field is required',
-                }, 
-                email: {
-                    required : 'Email field is required',
-                },
-                password: {
-                    required : 'Password field is required',
-                },
-
-            },
-            errorElement : 'span', 
-            errorPlacement: function (error,element) {
-                error.addClass('invalid-feedback');
-                element.closest('.form-controller').append(error);
-            },
-            highlight : function(element, errorClass, validClass){
-                $(element).addClass('is-invalid');
-            },
-            unhighlight : function(element, errorClass, validClass){
-                $(element).removeClass('is-invalid');
-            },
-        });
-    });
-    
-</script>
-
-</body>
-</html>
+  
