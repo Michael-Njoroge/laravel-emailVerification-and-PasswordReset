09683c60618da92ff6a97178710e6e120b75fb81
Michael-Njoroge <mikethecoder12@gmail.com>
2023-12-03 04:53:15 +0300

    create email verification

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -1,6 +1,10 @@
 @include('../header')
 <h1>Hello, <span class="name"></span></h1>
 
+<div class="email_verify">
+    <p><b>Email:- <span class="email"></span> &nbsp; <span class="verify"></span> </b></p>
+</div>
+
 <form action="">
 <input type="text" placeholder="Enter Name" name="email" id="name" required>
 <br><br>
@@ -19,8 +23,15 @@
                 if(data.status == 'true'){
                     console.log(data);
                     $(".name").text(data.data.name);
+                    $(".email").text(data.data.email);
                     $("#name").val(data.data.name);
                     $("#email").val(data.data.email);
+
+                    if(data.data.email_verified_at == null || data.data.email_verified_at == ''){
+                        $(".verify").html("<a href=''>Verify</a>");
+                    }else{
+                        $(".verify").text("Verified");
+                    }
                 }else{
                     alert(data.message);
                 }
