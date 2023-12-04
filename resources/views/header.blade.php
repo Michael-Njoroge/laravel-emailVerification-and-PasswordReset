4fa41666d7624f26421290a2838a5212f9a58b95
Michael-Njoroge <mikethecoder12@gmail.com>
2023-12-04 14:57:32 +0300

    complete refresh user UI

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -18,13 +18,29 @@
             right: 12px; 
             position: absolute;
           }
+          .refresh{
+            cursor: pointer;
+            background-color: rgb(213, 45, 20);
+            border-style: none;
+            border-radius: 25px;
+            padding: 5px 1px;
+            width: 115px;
+            color: #fff;
+            font-size: 15px;
+            font-weight: bold;
+            position: absolute;
+          }
     </style>
 </head>
 <body>
 
 <button class="logout">Logout</button>
+<button class="refresh">Refresh User</button>
+
   
 <script>
+
+    //redirect if token exists
     var token = localStorage.getItem('access_token');
 
     if( window.location.pathname == '/register' || window.location.pathname == '/')
@@ -35,6 +51,7 @@
         }
 
         $(".logout").hide();
+        $(".refresh").hide();
 
     }
     else{
@@ -64,7 +81,29 @@
                     }
                 }
             });
+
+            //refresh user
+            $(".refresh").click(function(){
+                $.ajax({
+                url: "http://127.0.0.1:8000/api/refresh-token",
+                type: "GET",
+                headers: {
+                    'Authorization':localStorage.getItem('access_token')
+                },
+                success: function(data){
+                    if(data.status == 'success'){
+                        localStorage.setItem("access_token",data.authorization+" "+data.token);
+                        alert('User Refreshed Successfully')
+                    }
+                    else{
+                        alert(data.message)
+                    }
+                }
+                });
+            });
     });
+
+
 </script>
 
 </body>
